b7da99ed17d39b7767ed109fba5cf5275bcb84f9
şükrü akkoyun <Sukruakkoyun87@hotmail.com>
2021-02-21 01:21:53 +0300

    Add Autofac,FluentValidation,AOP 2

--- Business/ValidationRules/FluentValidation/CarValidator.cs
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -12,7 +12,10 @@ namespace Business.ValidationRules.FluentValidation
         {
             RuleFor(c => c.CarName).NotEmpty();
             RuleFor(c => c.CarName).MinimumLength(2);
+            RuleFor(c => c.DailyPrice).NotEmpty();
             RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.ModelYear).NotEmpty();
+            RuleFor(c => c.Description).NotEmpty();
         }
     }
 }
